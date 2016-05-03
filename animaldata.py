c3144285d51012a5114e512eba8fc69a7f45a4b4
David Dasenbrook <david.dasenbrook@gmail.com>
2016-05-03 11:47:37 +0300

    create AnimalData class with preprocessing routines for the shelter animals data

--- animaldata.py
+++ animaldata.py
@@ -5,7 +5,7 @@
 import pandas
 
 class AnimalData(object):
-    def __init__(self, filename):
+    def __init__(self, filename=None):
         self.outcomeTypes = {
             'Return_to_owner': 0,
             'Euthanasia': 1,
@@ -31,31 +31,89 @@ class AnimalData(object):
             'Neutered Male': 1,
             'Unknown': 3
         }
+        self.colors = {
+            'Brown': 0,
+            'Cream': 1,
+            'Blue': 2,
+            'Tan': 3,
+            'Black': 4,
+            'Tabby': 5,
+            'Red': 6,
+            'White': 7,
+            'Silver': 8,
+            'Orange': 9,
+            'Chocolate': 10,
+            'Calico': 11,
+            'Torbie': 12,
+            'Yellow': 13,
+            'Tricolor': 14,
+            'Tortie': 15,
+            'Gray': 16,
+            'Buff': 17
+        }
+        self.ageUnitsInDays = {
+            'week': 7,
+            'month': 30,
+            'year': 365
+        }
         self.df = None
-        self.dfToLearn = None
-        self.readFile(filename)
+        if filename: self.readFile(filename)
 
 
     def readFile(self, filename):
+        """ read in the file using pandas and convert the data in all the fields
+        into numbers to make it ready for machine learning """
         self.df = pandas.read_csv(filename, header=0)
 
-        # we don't need the AnimalID, name or outcome subtype for learning
-        self.dfToLearn = self.df.drop(['AnimalID', 'Name', 'OutcomeSubtype'], axis=1)
+        # we don't need the name or outcome subtype for learning
+        self.df = self.df.drop(['Name', 'OutcomeSubtype'], axis=1)
 
-        # we will also drop the data, since it is not predictive. it is however a
+        # we will also drop the date, since it is not predictive. it is however a
         # source of data leakage and will lead to better results, although such
         # a model would not be very useful in practice
-        self.dfToLearn = self.dfToLearn.drop('DateTime', axis=1)
+        self.df = self.df.drop('DateTime', axis=1)
+
+        # There are too many different breeds. Group them by mixed / pure for now.
+        self.df['Mixed'] = self.df['Breed'].str.contains(r'Mix').map( { True: 1, False: 0 } )
+
+        # Same for the colors, so just match against a reasonable list of colors
+        regExp = '(' + '|'.join([s for s in self.colors.keys()]) + ')'
+        self.df['Color'] = self.df['Color'].str \
+                               .extract(regExp, expand=False).map(self.colors).fillna(18).astype(int)
+
+        # also map outcomeTypes, animalTypes, sexes and manipulations
+        self.df['OutcomeType'] = self.df['OutcomeType'].map(self.outcomeTypes)
+        self.df['AnimalType'] = self.df['AnimalType'].map(self.animalTypes)
+        self.df['Sex'] = self.df['SexuponOutcome'].map(self.sexes)
+        self.df['Manipulation'] = self.df['SexuponOutcome'].map(self.manipulations)
+
+        # convert age to age in days
+        self.df['AgeNumber'] = self.df['AgeuponOutcome'].str \
+                                   .extract('([0-9]+)', expand=False).astype(float)
+        regExp = '(' + '|'.join([s for s in self.ageUnitsInDays.keys()]) + ')'
+        self.df['AgeUnit'] = self.df['AgeuponOutcome'].str \
+                                  .extract(regExp, expand=False).map(self.ageUnitsInDays)
+        self.df['Age'] = self.df['AgeNumber'] * self.df['AgeUnit']
+
+        self.df = self.df.drop(['AgeuponOutcome', 'AgeNumber', 'AgeUnit', 'SexuponOutcome', 'Breed'],
+                               axis=1)
+
+    def makeArrays(self):
+        """ return a tuple of arrays: the data and the ids """
+        try:
+            return (self.df.drop('AnimalID', axis=1).values, self.df['AnimalID'].values)
+        except AttributeError:
+            print('Warning: the data frame has not been initialized yet, returning empty arrays')
+            return ([], [])
 
     @property
-    def dfToLearn(self):
-        return self.__dfToLearn
+    def df(self):
+        return self.__df
 
-    @dfToLearn.setter
-    def dfToLearn(self, dfToLearn):
-        self.__dfToLearn = dfToLearn
+    @df.setter
+    def df(self, df):
+        self.__df = df
 
 if __name__ == '__main__':
     a = AnimalData('data/train.csv')
-    print('Number of different breeds: %d' % len(a.dfToLearn['Breed'].unique()))
-    
+    print(a.makeArrays())
